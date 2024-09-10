850ca79a9e483cec35b7ff84962cfbfa9a5be3e3
goncaze <goncaze@gmail.com>
2024-09-10 17:49:12 -0300

    Corrige atributos

--- carrinho_compras/produto.py
+++ carrinho_compras/produto.py
@@ -1,28 +1,28 @@
 class Produto:
 
     def __init__(self, nome: str, preco: float = 0):
-        self.nome = nome
-        self.preco = preco
+        self._nome = nome
+        self._preco = preco
 
     @property
     def nome(self) -> str:
-        return self.nome
+        return self._nome
 
     @property
     def preco(self) -> float:
-        return self.preco
+        return self._preco
 
     @nome.setter
     def nome(self, novo_nome) -> None:
-        self.nome = novo_nome
+        self._nome = novo_nome
 
     @preco.setter
     def preco(self, novo_preco) -> None:
-        self.preco = novo_preco
+        self._preco = novo_preco
 
     def __str__(self) -> str:
         produto_impresso = f"""
-            {self.nome = }
-            {self.preco = }
+            {self._nome = }
+            {self._preco = }
         """
         return produto_impresso
---
+++ carrinho_compras/teste.py
@@ -0,0 +1,9 @@
+from produto import Produto
+
+sabonete = Produto("sabonete", 3.25)
+pao = Produto("Pão", 0.70)
+manteiga = Produto("Manteiga", 17.20)
+
+print(sabonete)
+print(pao)
+print(manteiga)
