a35def4fbb75260f02f22e92bd9fd7c5f587b731
goncaze <goncaze@gmail.com>
2024-09-10 17:40:18 -0300

    Carrinho de Compras

---
+++ carrinho_compras/produto.py
@@ -0,0 +1,21 @@
+class Produto:
+
+    def __init__(self, nome: str, preco: float = 0):
+        self.nome = nome
+        self.preco = preco
+
+    @property
+    def nome(self) -> str:
+        return self.nome
+
+    @property
+    def preco(self) -> float:
+        return self.preco
+
+    @nome.setter
+    def nome(self, novo_nome) -> None:
+        self.nome = novo_nome
+
+    @preco.setter
+    def preco(self, novo_preco) -> None:
+        self.preco = novo_preco
