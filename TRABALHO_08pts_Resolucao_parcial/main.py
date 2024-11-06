f47c2413b75b8d3a021fda22673e1b54debeaf37
goncaze <goncaze@gmail.com>
2024-11-06 16:55:53 -0300

    De função principal para classe principal

--- TRABALHO_08pts_Resolucao_parcial/main.py
+++ TRABALHO_08pts_Resolucao_parcial/main.py
@@ -2,43 +2,45 @@ import os
 from src.controller.data_categoria_controller import DataCategoriaController
 from src.controller.data_controller import DataController
 
-data_cat_ctlr = DataCategoriaController()
-data_ctlr = DataController()
 
-menu_p = """
-    ----------------------------------------------
-    Menu principal de operações
-    ----------------------------------------------
-    [1] – Operações com categorias de data
-    [2] - Operações com datas
-    [3] - Operações com categorias de evento
-    [4] - Operações com categorias de evento_data
-    [0] – Encerrar aplicativo
-  """
+class Principal:
+    def __init__(self):
+        self.data_cat_ctlr = DataCategoriaController()
+        self.data_ctlr = DataController()
+        self.menu_p = """
+        ----------------------------------------------
+        Menu principal de operações
+        ----------------------------------------------
+        [1] – Operações com categorias de data
+        [2] - Operações com datas
+        [3] - Operações com categorias de evento
+        [4] - Operações com categorias de evento_data
+        [0] – Encerrar aplicativo
+        """
 
+    def menu_principal(self) -> None:
 
-def menu_principal(menu_p: str) -> None:
+        while True:
+            # Limpar a tela
+            os.system("cls")
 
-    while True:
-        # Limpar a tela
-        os.system("cls")
+            print(self.menu_p)
+            print()  # Linha vazia
+            selecao = input("\tOpção: ")
 
-        print(menu_p)
-        print()  # Linha vazia
-        selecao = input("Opção: ")
-
-        match selecao:
-            case "0":  # [0] – Encerrar aplicativo
-                break
-            case "1":  # [1] – Operações com categorias de data
-                data_cat_ctlr.menu_data_categoria()
-            case "2":  # [2] - Operações com datas
-                data_ctlr.menu_data()
-            case "3":  # [3] - Operações com categorias de evento
-                pass
-            case "4":  # [4] - Operações com categorias de evento_data
-                pass
+            match selecao:
+                case "0":  # [0] – Encerrar aplicativo
+                    break
+                case "1":  # [1] – Operações com categorias de data
+                    self.data_cat_ctlr.menu_data_categoria()
+                case "2":  # [2] - Operações com datas
+                    self.data_ctlr.menu_data()
+                case "3":  # [3] - Operações com categorias de evento
+                    pass
+                case "4":  # [4] - Operações com categorias de evento_data
+                    pass
 
 
 if __name__ == "__main__":
-    menu_principal(menu_p)
+    p = Principal()
+    p.menu_principal()
